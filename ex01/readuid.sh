8db4e16daee02f2ebaaa29b88ed2705632c86994
Karl Wolf <wolfkarl@gmail.com>
2013-10-21 21:54:21 +0200

    UID Script unvollstaenidg

--- ex01/readuid.sh
+++ ex01/readuid.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 STUB="https://hpi-vdb.de/vulndb/isec_task/?uid="
-GET="karl.wolf"
+DEFAULTGET="karl.wolf"
+
+if [ -n "$1" ]
+# Test whether command-line argument is present (non-empty).
+then
+  GET=$1
+else  
+  GET=$DEFAULTGET # Default, if not specified on command-line.
+fi 
 
 
-echo $STUB
+# fetch all text from personal website
+# -s suppresses statistics, so only the website's content is saved
+raw=$(curl -s $STUB$GET)
+
+#echo $raw
+uid=$(echo $raw | grep (karl))
+
+echo $uid 
